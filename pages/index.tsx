9dec42a44bc9ed73f5962f8adac5e4ab7386cbc6
Gabriel <gabrielm.linassi@gmail.com>
2021-09-27 15:31:50 -0300

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ export default function Home() {
         <Roles />
         <HoWeWork />
         <WhyWorkWithUs />
-        <MoreReasons />
+        {/* <MoreReasons /> */}
         <SubFooter />
         <Footer />
       </main>
